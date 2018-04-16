faca386cee315896f15e2653f2137810e0b2ff6f
Mikeypdog <mikeypdog911@gmail.com>
2018-04-16 12:30:02 +0100

    First commit

---
+++ MouseModGlobals.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Harmony;
+
+namespace MouseMod
+{
+    public static class MouseModGlobals
+    {
+        public static bool MouseAcceleration = true;
+        public static int MouseSmoothingSteps = 5;
+
+        public static void LoadFromFile()
+        {
+            var dic = File.ReadAllLines("mods/MouseMod.txt")
+                        .Where(l => !l.StartsWith("/"))
+                        .Select(l => l.Split(new[] { '=' }))
+                        .Where(arr => arr.Length == 2)
+                        .ToDictionary(s => s[0].Trim(), s => s[1].Trim());
+
+            SetGlobal<int>(dic, "MouseSmoothingSteps", x => MouseSmoothingSteps = x);
+            SetGlobal<bool>(dic, "MouseAcceleration", x => MouseAcceleration = x);
+
+            var leftoverEntries = dic.Select(kvp => kvp.Key + "=" + kvp.Value).ToArray();
+            if (leftoverEntries.Any())
+            {
+                var leftoverString = string.Join(", ", leftoverEntries);
+                FileLog.Log("*** LINES FOUND WITHOUT MATCH :" + leftoverString);
+            }
+
+            FileLog.Log("Finished loading MouseMod values.");
+        }
+
+        public static void SetGlobal<T>(Dictionary<string, string> dict, string key, Action<T> globalSetter)
+        {
+            string value;
+            if (!dict.TryGetValue(key, out value))
+            {
+                FileLog.Log("* No entry for '" + key + "' found. Defaulting value.");
+                return;
+            }
+
+            try
+            {
+                T val = typeof(T).IsEnum 
+                    ? ParseToEnum<T>(value) 
+                    : ParseTo<T>(value);
+
+                FileLog.Log("* Setting " + key + " to " + val);
+                dict.Remove(key);
+                globalSetter(val);
+            }
+            catch (Exception e)
+            {
+                FileLog.Log("*** BAD VALUE for '" + key + "' ('" + value + "'). Defaulting value. Full error below:");
+                FileLog.Log(e.Message);
+            }
+        }
+
+        public static T ParseTo<T>(object obj)
+        {
+            return (T)Convert.ChangeType(obj, typeof(T));
+        }
+
+        public static T ParseToEnum<T>(string input)
+        {
+            return (T)Enum.Parse(typeof(T), input);
+        }
+    }
+}
---
+++ PatchMouseMovement.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Harmony;
+using UnityEngine;
+
+namespace MouseMod
+{
+    [HarmonyPatch(typeof (vp_FPSCamera))]
+    [HarmonyPatch("GetMouseDelta")]
+    class PatchMouseMovement
+    {
+        static bool Prepare()
+        {
+            FileLog.Log("");
+            FileLog.Log(DateTime.Now + " ---- Loading Mouse Mod.");
+            try
+            {
+                MouseModGlobals.LoadFromFile();
+            }
+            catch (Exception e)
+            {
+                Debug.LogFormat(e.Message);
+                FileLog.Log(e.Message);
+                throw;
+            }
+
+            return true;
+        }
+
+        static void Prefix(vp_FPSCamera __instance)
+        {
+            if (__instance.MouseAcceleration != MouseModGlobals.MouseAcceleration)
+            {
+                var msg = " ** MouseMod: MouseAcceleration was " + __instance.MouseAcceleration + ". Changing to " +
+                          MouseModGlobals.MouseAcceleration;
+                FileLog.Log(msg);
+                __instance.MouseAcceleration = MouseModGlobals.MouseAcceleration;
+            }
+
+            if (__instance.MouseSmoothSteps != MouseModGlobals.MouseSmoothingSteps)
+            {
+                var msg = " ** MouseMod: MouseSmoothSteps was " + __instance.MouseSmoothSteps + ". Changing to " +
+                          MouseModGlobals.MouseSmoothingSteps;
+                FileLog.Log(msg);
+                __instance.MouseSmoothSteps = MouseModGlobals.MouseSmoothingSteps;
+            }
+        }
+    }
+}
